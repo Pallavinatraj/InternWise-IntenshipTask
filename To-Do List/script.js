d6ff3e8fb8ec69f20b0db5d34f9709463e947ffc
Pallavinatraj <pallavinatraj31@gmail.com>
2024-11-25 16:51:42 +0530

    To-Do List

---
+++ To-Do List/script.js
@@ -0,0 +1,11 @@
+document.querySelector("#green").addEventListener("click", greenButtonFunction)
+
+
+
+function greenButtonFunction() {
+    let list = document.createElement("ul");
+    document.querySelector('#box1').appendChild(list);
+
+
+
+}
